cac00c7a99125a848909c286ef90cae375bb9dff
Yuki-Ikegami <fp25657.hawks@icloud.com>
2018-05-03 11:49:52 +0900

    first commit

---
+++ skateSNS/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  skateSNS
+//
+//  Created by IkegamiYuki on 2018/05/01.
+//  Copyright © 2018 IkegamiYuki. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
